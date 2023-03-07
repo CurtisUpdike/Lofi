d7559b6bc222e9d8c8afe9c7d7f0ba062988ee82
Curtis Updike <curtis@updike.dev>
2023-03-07 18:06:31 -0500

    Use AppleDeveloperToken package

--- Services/TokenProvider.cs
+++ Services/TokenProvider.cs
@@ -1,65 +1,25 @@
-﻿using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Cryptography;
+﻿using AppleDeveloperToken;
 
 namespace Lofi.Services;
 
 public class TokenProvider : ITokenProvider
 {
-	private string _teamId;
-	private string _keyId;
-	private string _authKey;
+	private readonly TokenGenerator _tokenGenerator;
 
 	public string Token
 	{
 		get
 		{
-			return CreateToken(LoadPrivateKey(), new TimeSpan(2, 0, 0)); ;
+			return _tokenGenerator.Generate(TimeSpan.FromDays(30));
 		}
 	}
 
 	public TokenProvider(IConfiguration config)
 	{
-		if (config["Lofi:TeamId"] == null) { throw new Exception("TeamId Configuration Not Found"); }
-		if (config["Lofi:KeyId"] == null) { throw new Exception("KeyId Configuration Not Found"); }
-		if (config["Lofi:AuthKey"] == null) { throw new Exception("AuthKey Configuration Not Found"); }
+		var privateKey = config["Lofi:AuthKey"] ?? string.Empty;
+		var teamId = config["Lofi:TeamId"] ?? string.Empty;
+		var keyId = config["Lofi:KeyId"] ?? string.Empty;
 
-		_teamId = config["Lofi:TeamId"]!;
-		_keyId = config["Lofi:KeyId"]!;
-		_authKey = StripKey(config["Lofi:AuthKey"]!);
-	}
-
-	private string StripKey(string keyFromConfig)
-	{
-		return keyFromConfig
-			.Replace("-----BEGIN PRIVATE KEY-----", "")
-			.Replace("-----END PRIVATE KEY-----", "")
-			.Replace("\n", "");
-	}
-
-	private ECDsa LoadPrivateKey()
-	{
-		var privateKey = Convert.FromBase64String(_authKey);
-		var privateKeyECDsa = ECDsa.Create();
-		privateKeyECDsa.ImportPkcs8PrivateKey(privateKey, out _);
-		return privateKeyECDsa;
-	}
-
-	private string CreateToken(ECDsa ecdsa, TimeSpan timeSpan)
-	{
-		var now = DateTime.UtcNow;
-		var tokenHandler = new JwtSecurityTokenHandler();
-
-		var token = tokenHandler.CreateJwtSecurityToken(
-			issuer: _teamId,
-			subject: null,
-			notBefore: now,
-			expires: now.Add(timeSpan),
-			issuedAt: now,
-			signingCredentials: new SigningCredentials(
-				new ECDsaSecurityKey(ecdsa) { KeyId = _keyId },
-				SecurityAlgorithms.EcdsaSha256));
-
-		return tokenHandler.WriteToken(token);
+		_tokenGenerator = new TokenGenerator(privateKey, teamId, keyId);
 	}
 }
